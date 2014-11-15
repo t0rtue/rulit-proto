c4e69588641f972a59aa6231a108afc5e49c3351
T0rtue <turtlelab@free.fr>
2014-11-15 15:54:45 +0100

    Bootstrap theme roller

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ angular.module('rulit', ['ui.bootstrap', 'ri.module.rulebook', 'ri.module.token'
 .controller('rulit.controller', function() {
     this.layout = {
         mode : 2
-    }
+    };
+
+    this.bootstrapTheme='standard';
 })
 
 ;
