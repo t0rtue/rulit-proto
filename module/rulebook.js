933458a83d3d57167379f617f7a8b467ed59dcf3
t0rtue <turtlelab@free.fr>
2014-12-05 09:36:07 +0100

    Move some directive definition from old to new rulebook module

--- module/rulebook.js
+++ module/rulebook.js
@@ -32,50 +32,6 @@ angular.module('ri.module.rulebook', ['ui.router'])
         })
 })
 
-.controller('TokenEditModalCtrl', function ($scope, $modal, $log) {
 
-  $scope.openModal = function (token) {
-
-    var modalInstance = $modal.open({
-      templateUrl: 'module/token/tokenEditor.html',
-      controller: 'TokenEditModalInstanceCtrl',
-      size : 'lg',
-      resolve: {
-        token: function() {return token}
-      }
-    });
-
-    modalInstance.result.then(function (tokenview) {
-      token.view = tokenview;
-    }, function () {
-    });
-  };
-})
-
-.controller('TokenEditModalInstanceCtrl', function ($scope, $modalInstance, token) {
-
-  $scope.token = token;
-  $scope.tokenView = angular.copy(token.view);
-
-
-  $scope.ok = function (tokenview) {
-    $modalInstance.close(tokenview);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-})
-
-.directive('rbTodo', function() {
-    return {
-        restrict : 'E',
-        scope : {
-            'type' : '@'
-        },
-        transclude : true,
-        template :  '<div class="panel panel-info"><div class="panel-heading">TODO {{type}}</div><div class="panel-body" ng-transclude></div></div>',
-    }
-})
 
 ;
